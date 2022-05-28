564a9f05effddaab17bdb19bc4e17fd16ea7922b
Benson Ndiwa <bnkimtai@gmail.com>
2022-05-28 12:43:52 +0300

    full

--- src/Posttweet.js
+++ src/Posttweet.js
@@ -1,50 +1,53 @@
 import Profile from './images/andrew.jpg';
 import {useState, useEffect} from 'react';
-import {useHistory} from 'react-router-dom';
 import Content from './Content';
 
 const Posttweet = () => {
         
-    const [tweet, setTweet] = useState('');
+    const [tweet, setPost] = useState('');
 
-    const tweets = {tweet}
+    const posts = {tweet}
 
-    const history = useHistory();
 
     const handleSubmit = (e)=>{
         e.preventDefault();
-
+        e.target.value=null;
         fetch('http://localhost:8001/tweets',{
             method: 'POST',
             headers: {'Content-Type':'application/json'},
-            body: JSON.stringify(tweets)
+            body: JSON.stringify(posts)
         })
         .then(()=>{
                   
         })
+        
     }
 
+   
+
     return ( 
-        <div className="post">
-            <div className="profile">
-                <img src={Profile} alt="" style={{borderRadius:"100%", marginRight:'2%', height: "50px"}}/>
-            </div>
-            <div className="tweet">
-                <form onSubmit={handleSubmit}>
-                    <textarea 
-                        placeholder="What's happening?" 
-                        rows='2' cols='38' 
-                        required
-                        onChange={(e) => setTweet(e.target.value)}
-                        style={{resize: 'none',marginLeft: '1%', border: 'none', borderBottom: '1px solid gray', backgroundColor: 'black', color: 'white', fontSize: '22px'}}></textarea>
-                    <br />
-                    <input type="submit" style={{marginTop:'1%',backgroundColor:'rgb(29, 155, 240)', color: 'white', padding: '10px', border: 'none', borderRadius:'9999px', fontWeight: 'bold', fontSize: '16px', float: 'right'}} value="Tweet" />
-                    </form>
-                
-            </div>
-            
-            
-        </div>
+            <div className="post">
+                        <div className="profile">
+                            <img src={Profile} alt="" style={{borderRadius:"100%", marginRight:'2%', height: "50px"}}/>
+                        </div>
+                        <div className="tweet">
+                            <form onSubmit={handleSubmit}>
+                                <textarea 
+                                    placeholder="What's happening?" 
+                                    rows='2' cols='38' 
+                                    required
+                                    onChange={(e) => setPost(e.target.value)}
+                                    style={{resize: 'none',marginLeft: '1%', border: 'none', borderBottom: '1px solid gray', backgroundColor: 'black', color: 'white', fontSize: '22px'}}></textarea>
+                                <br />
+                                <input type="submit" style={{marginTop:'1%',backgroundColor:'rgb(29, 155, 240)', color: 'white', padding: '10px', border: 'none', borderRadius:'9999px', fontWeight: 'bold', fontSize: '16px', float: 'right'}} value="Bweet" />
+                                </form>
+                            
+                        </div>
+                        
+                        
+                    </div>
+        
+        
      );
 }
  
